ac201f4d235f9fc4c50e563edf38392a03495737
Meikul <meikul.harris@gmail.com>
2015-10-10 17:46:11 -0600

    moving
    moving old view into old views folder. creating buttons, getting rid of
    header image, button js

---
+++ oldViews/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	var centerPx = $(window).width()/2;
+	$('#signIn').css('left',centerPx-101.5);
+	$('#signUp').css('left',centerPx+1.5);
+	$('.buttons').mouseenter(function(){
+		$(this).animate({opacity:'0.6'}, 200);
+	});
+	$('.buttons').mouseleave(function(){
+		$(this).animate({opacity:'1'}, 200);
+	});
+});
